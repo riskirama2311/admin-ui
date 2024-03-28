8af29f2e3b18dfd597187b2f14d0bf2bd1b3687a
riskirama2311 <111202113939@mhs.dinus.ac.id>
2024-03-28 20:18:50 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h3>Riski Ramadhani Putradi</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
