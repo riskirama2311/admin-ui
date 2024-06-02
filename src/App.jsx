97b04fd7a0f37b9452e994334eebb82173968a24
riskirama2311 <111202113939@mhs.dinus.ac.id>
2024-06-02 18:42:15 +0700

    State Management

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formsource";
 import Mylist from "./pages/MyList/Mylist";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
   return (
-    <>
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
@@ -30,7 +35,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,8 +8,12 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PortraitIcon from "@mui/icons-material/Portrait";
 import LogoutIcon from "@mui/icons-material/Logout";
 import CategoryIcon from "@mui/icons-material/Category";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -63,8 +67,8 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
+        <div className="colorOption" onClick={() => dispatch({ type: "LIGHT"})}></div>
+        <div className="colorOption" onClick={() => dispatch({ type: "DARK"})}></div>
       </div>
     </div>
   );
