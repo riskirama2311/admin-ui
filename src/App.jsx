f1278224084c8e3fba4b2d01efd51ae5959c30ad
riskirama2311 <111202113939@mhs.dinus.ac.id>
2024-06-04 19:02:12 +0700

    AUTH FIREBASE

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,41 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formsource";
 import Mylist from "./pages/MyList/Mylist";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+
+  const { currentUser } = useContext(AuthContext)
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
+
+  const NotRequireAuth = ({ children }) => {
+    return currentUser ? <Navigate to="/login" /> : children;
+  };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />}></Route>
-            <Route path="login" element={<Login />}></Route>
+            <Route path="login" element={<NotRequireAuth><Login /></NotRequireAuth>}></Route>
+            <Route index element={<RequireAuth><Home /></RequireAuth>}></Route>
             <Route path="users">
-              <Route index element={<List />}></Route>
-              <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New inputs={userInputs} title="Add New User" />}></Route>
+              <Route index element={<RequireAuth><List /></RequireAuth>}></Route>
+              <Route path=":userId" element={<RequireAuth><Single /></RequireAuth>}></Route>
+              <Route
+                path="new"
+                element={<RequireAuth><New inputs={userInputs} title="Add New User" /></RequireAuth>}
+              />
             </Route>
             <Route path="products">
               <Route index element={<List />}></Route>
---
+++ src/firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: "store-tutorial-d1cb6.firebaseapp.com",
+    projectId: "store-tutorial-d1cb6",
+    storageBucket: "store-tutorial-d1cb6.appspot.com",
+    messagingSenderId: "733431122470",
+    appId: "1:733431122470:web:aa9ecec8a37349640174e7"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
